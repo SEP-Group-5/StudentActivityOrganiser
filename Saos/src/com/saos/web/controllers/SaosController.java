2b1b8be200a576bcf85ae970e78ec0964f41e5de
bernicecc <bernicecc7@gmail.com>
2015-09-26 21:36:01 +1000

    Spring mvc skeleton The structure has been setup. It contains all the jar files required for spring mvc and added dependencies. Also created a simple program and it works All controllers go to Java Resources -> src folder, all views go to WEB-INF->jsps folder

---
+++ Saos/src/com/saos/web/controllers/SaosController.java
@@ -0,0 +1,22 @@
+package com.saos.web.controllers;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SaosController {
+	
+	@RequestMapping("/")
+	public ModelAndView showHome(){
+		
+		ModelAndView mv = new ModelAndView("home");
+		Map<String, Object> model = mv.getModel();
+		model.put("name", "River");
+		return mv;
+	}
+}
